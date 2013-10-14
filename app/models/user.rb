052998a62841ad11b5ec4705aa0c6d1e99d4f0b2
Freddy Rodriguez <freddy@kangomedia.com>
2013-10-14 15:18:17 -0500

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  has_secure_password
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name,  presence: true, length: { maximum: 50 }
@@ -7,6 +6,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
   validates :password, length: { minimum: 6 }
   
   def User.new_remember_token
